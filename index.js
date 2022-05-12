5bc944419582312b008b2aa173302f383d1257d9
D.M.J.C Dissanayake <it18226324@my.sliit.lk>
2022-05-13 01:29:54 +0530

    mongo env setup

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const mogoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/congig");
 
 const app = express();
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 
 mongoose
-.connect("mongodb://janaka:mypassword@mongo:27017/?authSource=admin")
+.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+})
 .then(() => console.log("Successfully connected to db!"))
 .catch((e) => console.log(e));
 
