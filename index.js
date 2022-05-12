7d51b6ea14acb1d4f845bdb2079a292c05f72b5c
D.M.J.C Dissanayake <it18226324@my.sliit.lk>
2022-05-13 01:01:48 +0530

    mongo db config successfuly

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require("express")
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const mogoose = require("mongoose");
 
-const app = express()
+const app = express();
+
+mongoose
+.connect("mongodb://janaka:mypassword@mongo:27017/?authSource=admin")
+.then(() => console.log("Successfully connected to db!"))
+.catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hi There..!</h2>")
-})
+    res.send("<h2>Hi There..!</h2>");
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Listning on port ${port}`))
+app.listen(port, () => console.log(`Listning on port ${port}`));
